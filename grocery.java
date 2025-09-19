856ad8ef3a97dece4283b19f00f98366d8df14c1
Abin Joy <abinjoy1410@gmail.com>
2025-09-19 15:07:04 +0530

    developing a simple software for a zoo to demonstrate animal behaviors.

--- grocery.java
+++ grocery.java
@@ -9,7 +9,7 @@ public class grocery {
         }
         if (total > 500) {
             double discount = total * 0.10;
-            System.out.printf("Discount Applied: Rs.%.2f\n", discount);
+            System.out.printf(" Discount Applied: Rs.%.2f\n", discount);
             total -= discount;
         }
         return total;
@@ -18,33 +18,25 @@ public class grocery {
          String[] items = {"Rice", "Sugar", "Oil", "Soap", "Milk"};
         int[] prices = {50, 40, 100, 25, 30};
         int[] quantities = new int[items.length];
-
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Welcome to the Grocery Store!");
         System.out.println("Available items and prices (per unit):");
         for (int i = 0; i < items.length; i++) {
             System.out.println(items[i] + " - Rs." + prices[i]);
         }
-
         try {
             for (int i = 0; i < items.length; i++) {
                 System.out.print("Enter quantity for " + items[i] + "(per units): ");
                 quantities[i] = Integer.parseInt(scanner.nextLine());
             }
-
             double finalAmount = calculateTotal(quantities, prices);
             System.out.printf("Final Amount to Pay: Rs.%.2f\n", finalAmount);
-
-            // 🕒 Display current date and time
             LocalDateTime now = LocalDateTime.now();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
             System.out.println("Purchase Date & Time: " + now.format(formatter));
-
-        } catch (NumberFormatException e) {
+            } catch (NumberFormatException e) {
             System.out.println(" Invalid input! Please enter numeric values only.");
         }
-
         scanner.close();
     }
 }
